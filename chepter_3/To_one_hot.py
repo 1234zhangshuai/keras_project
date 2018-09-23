3d7a01344e9feacd6cbd86936be5717dff3ee06d
1234zhangshuai <564329378@qq.com>
2018-09-23 10:46:56 +0800

    remove

--- chepter_3/To_one_hot.py
+++
@@ -1,9 +0,0 @@
-
-import numpy as np
-
-def to_one_hot(labels, dimension=46):
-    results = np.zeros((len(labels), dimension))
-    for i, label in enumerate(labels):
-        results[i, label] = 1.
-    return results
-

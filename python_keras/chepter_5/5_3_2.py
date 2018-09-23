8fb4986799abc3954dd8c95579f904a5499ed9f1
1234zhangshuai <564329378@qq.com>
2018-09-23 11:15:13 +0800

    add file

--- python_keras/chepter_5/5_3_2.py
+++ python_keras/chepter_5/5_3_2.py
@@ -9,3 +9,8 @@ conv_base = VGG16(weights='imagenet', include_top=False, input_shape=(150, 150, 
 # 在卷积上添加一个密集连接分类器
 model = models.Sequential()
 model.add(conv_base)
+# 展平
+model.add(layers.Flatten())
+model.add(layers.Dense(256, activation='relu'))
+model.add(layers.Dense(1, activation='sigmoid'))
+# model.summary()
